629383fed73ceab82d8cdd82b2c9ad7541841384
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2012-02-13 00:06:13 -0500

    cleaned up options code, improved documentation

--- jquery.stickwith.js
+++ jquery.stickwith.js
@@ -1,26 +1,33 @@
+/* Stick With takes absolutely positioned elements and changes them to 
+ *   fixed position elements before they are about to scroll off the top
+ *   of the page. When the user scrolls back up they become absolutely
+ *   positioned again.
+ * Options: top - the minimum number of pixels the element should
+ *                stay away from the top of the viewport. */
 (function ( $ ) {
    $.fn.stickWith = function ( options ) {
       
       var settings = $.extend({
-         top: 0,
+         top: 0,   // css top property. used when element's position is fixed
       }, options);
       
       return this.each(function () {
          var $this    = $( this );
          var $window  = $( window );
 
-         var swapPos  = $this.offset().top - settings.top; 
+	 // number of pixels down the page we should change to fixed position
+         var swapPos  = $this.offset().top - settings.top;
          var fixed    = false;
          var fixedCSS = {position: 'fixed',
                          top:       settings.top,
-       		  left:      $this.offset().left };
+       	                 left:      $this.offset().left };
          var absoluteCSS = {position: 'absolute',
                             top:      $this.css('top'),
-       		     left:     $this.css('left'),
+                            left:     $this.css('left'),
                             right:    $this.css('right'),
                             bottom:   $this.css('bottom') }; 
 
-         $window.scroll(function () { //called often, must be efficient 
+         $window.scroll(function () {  //called often, must be efficient 
             var shouldBeFixed = $window.scrollTop() > swapPos;
             if (shouldBeFixed && !fixed) {
                fixed = true;
